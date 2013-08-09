02ca42dd29ef8efe5d7a8b9b1390b7c778d4cab4
Josh Duffy <jduffy@mines.edu>
2013-08-09 17:32:00 -0600

    Updated RSPEC tests

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Josh Duffy"
+    email    "jduffy@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -102,4 +102,9 @@ describe User do
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
