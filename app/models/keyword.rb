8590ac66b275a1deb6aeaa660c63b31c887f7f98
Josh Duffy <jduffy@mines.edu>
2013-08-20 16:06:27 -0600

    updates

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -8,11 +8,11 @@ class Keyword < ActiveRecord::Base
 		end
 	end
 
-	def self.import(file)
+	def self.import(file, campaign_name)
 		CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
 			row[:ad_group] << " 1 Exact"
 			row << ["keyword_type", "Exact"]
-			row << ["campaign_name", "Example Campaign"]
+			row << ["campaign_name", campaign_name]
 			Keyword.create! row.to_hash
 		end
 		CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
