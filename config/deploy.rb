c1d6ece30799e78ecd2aa621a196b2be0aea4376
Josh Duffy <jduffy@mines.edu>
2013-10-11 23:23:59 -0600

    updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ default_run_options[:pty] = true  # Must be set for the password prompt
 set :application, 'susclicks'
 set :scm, :git
 set :user, 'josh'
-set :use_sudo, false
+set :use_sudo, true
 set :repository, 'git@github.com:duffcodester/#{application}.git'
 set :branch, 'master'
 set :scm_passphrase, 'Coppermtn7'
@@ -24,6 +24,41 @@ set :keep_releases, 5
 
 set :stages, ['production']
 set :default_stage, 'production'
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
+end
 
 # role :web, '192.241.220.72'                          # Your HTTP server, Apache/etc
 # role :app, '192.241.220.72'                          # This may be the same as your `Web` server
